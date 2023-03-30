33c18762a6fec4c3ab7fe53e8eb3df97aa162813
xinking129 <xinxin13@huawei.com>
2023-03-30 20:54:03 +0800

    Solve the problem of non execution of arkui task
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- ability/ability_runtime/cross_platform/frameworks/native/base/event_handler/src/event_runner.cpp
+++ ability/ability_runtime/cross_platform/frameworks/native/base/event_handler/src/event_runner.cpp
@@ -553,6 +553,10 @@ uint64_t EventRunner::GetThreadId()
 
 bool EventRunner::IsCurrentRunnerThread()
 {
+   if (PlatformEventRunner::CheckCurrent()) {
+        return true;
+    }
+
     return std::this_thread::get_id() == innerRunner_->GetThreadId();
 }
 
