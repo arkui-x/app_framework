f2f0c50a11d105b4643d6de93ff1749f9a66fd4b
xinking129 <xinxin13@huawei.com>
2023-07-06 21:04:13 +0800

    fix register name
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- ability/ability_runtime/cross_platform/frameworks/js/napi/configuration_constant/configuration_constant_module.cpp
+++ ability/ability_runtime/cross_platform/frameworks/js/napi/configuration_constant/configuration_constant_module.cpp
@@ -86,7 +86,7 @@ static napi_module _module = { .nm_version = 1,
     .nm_priv = (static_cast<void*>(0)),
     .reserved = { 0 } };
 
-extern "C" __attribute__((constructor)) void RegisterModule(void)
+extern "C" __attribute__((constructor)) void NAPI_app_ability_ConfigurationConstant_AutoRegister(void)
 {
     napi_module_register(&_module);
 }
