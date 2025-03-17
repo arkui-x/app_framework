37b423fb021e2e271132c42df28bf3b06a02a78a
quguiren <quguiren@huawei.com>
2025-03-17 19:59:18 +0800

    add OHMUrl
    Signed-off-by: quguiren <quguiren@huawei.com>

--- ability/ability_runtime/cross_platform/frameworks/native/base/bundle_parser/include/common_profile.h
+++ ability/ability_runtime/cross_platform/frameworks/native/base/bundle_parser/include/common_profile.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -452,6 +452,8 @@ constexpr const char* MODULE_ATOMIC_SERVICE_PRELOADS = "preloads";
 // module atomicService preloads
 constexpr const char* PRELOADS_MODULE_NAME = "moduleName";
 extern thread_local int32_t parseResult;
+
+constexpr const char* MODULE_PACKAGE_NAME  = "packageName";
 } // namespace Profile
 } // namespace AppExecFwk
 } // namespace OHOS
