e6d57a72b25484494896d0e478451254f8b54412
jinpeng0804 <jinpeng0804@vip.qq.com>
2025-03-31 20:54:41 +0800

    add dfx code
    Signed-off-by: jinpeng0804 <jinpeng0804@vip.qq.com>
    Change-Id: I70ecc49a48dd08b664821d0dbf9442437d7ea1c5

--- ability/ability_runtime/cross_platform/interfaces/inner_api/jsruntime/js_error_utils.h
+++ ability/ability_runtime/cross_platform/interfaces/inner_api/jsruntime/js_error_utils.h
@@ -22,7 +22,10 @@
 namespace OHOS {
 namespace AbilityRuntime {
 void ThrowError(napi_env env, int32_t errCode, const std::string& errorMsg = "");
+void ThrowError(napi_env env, const AbilityErrorCode& err);
+void ThrowInvalidCallerError(napi_env env);
 void ThrowTooFewParametersError(napi_env env);
+void ThrowInvalidNumParametersError(napi_env env);
 void ThrowNoPermissionError(napi_env env, const std::string& permission);
 void ThrowErrorByNativeErr(napi_env env, int32_t err);
 void ThrowInvalidParamError(napi_env env, const std::string& message);
