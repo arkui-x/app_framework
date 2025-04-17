c6acb75cc6cecdfbd568a84a430dbae596266183
zhangqing <zhangqing50@h-partners.com>
2025-04-17 15:19:03 +0800

    arkui-x update master OH to weekly_20250414 and arkui-x fix build bug
    Signed-off-by: zhangqing <zhangqing50@h-partners.com>
    Change-Id: I5e2765937291af0c6e222bbba03f29c80c1e628d

--- graphic_2d/ios/rs_render_service_client.mm
+++ graphic_2d/ios/rs_render_service_client.mm
@@ -149,8 +149,7 @@ bool RSRenderServiceClient::TakeSurfaceCapture(NodeId id, std::shared_ptr<Surfac
     return false;
 }
 
-int32_t RSRenderServiceClient::SetFocusAppInfo(
-    int32_t pid, int32_t uid, const std::string &bundleName, const std::string &abilityName, uint64_t focusNodeId)
+int32_t RSRenderServiceClient::SetFocusAppInfo(const FocusAppInfo& info)
 {
     return false;
 }
