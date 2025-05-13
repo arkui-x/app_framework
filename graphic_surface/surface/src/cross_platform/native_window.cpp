f70583455660d5c6abbf1a3d65b092893993b766
quguiren <quguiren1@huawei-partners.com>
2025-05-13 11:20:09 +0800

    add graphic_surface
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

---
+++ graphic_surface/surface/src/cross_platform/native_window.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "external_window.h"
+#include "surface_utils.h"
+
+using namespace OHOS;
+int32_t OH_NativeWindow_CreateNativeWindowFromSurfaceId(uint64_t surfaceId, OHNativeWindow **window)
+{
+    if (window == nullptr) {
+        return OHOS::SURFACE_ERROR_INVALID_PARAM;
+    }
+
+    auto &utils = SurfaceUtils::GetInstance();
+    *window = reinterpret_cast<OHNativeWindow *>(utils.GetNativeWindow(surfaceId));
+    if (*window != nullptr) {
+        return OHOS::GSERROR_OK;
+    }
+    return OHOS::SURFACE_ERROR_INVALID_PARAM;
+}
---
+++ graphic_surface/surface/src/cross_platform/surface_utils.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cinttypes>
+#include "surface_utils.h"
+
+namespace OHOS {
+SurfaceUtils& SurfaceUtils::GetInstance()
+{
+    static SurfaceUtils instance;
+    return instance;
+}
+
+SurfaceUtils::~SurfaceUtils()
+{
+    nativeWindowCache_.clear();
+}
+
+void* SurfaceUtils::GetNativeWindow(uint64_t uniqueId)
+{
+    std::lock_guard<std::mutex> lockGuard(nativeWindowLock_);
+    auto iter = nativeWindowCache_.find(uniqueId);
+    if (iter == nativeWindowCache_.end()) {
+        return nullptr;
+    }
+    return iter->second;
+}
+
+SurfaceError SurfaceUtils::AddNativeWindow(uint64_t uniqueId, void *nativeWindow)
+{
+    if (nativeWindow == nullptr) {
+        return GSERROR_INVALID_ARGUMENTS;
+    }
+    std::lock_guard<std::mutex> lockGuard(nativeWindowLock_);
+    if (nativeWindowCache_.count(uniqueId) == 0) {
+        nativeWindowCache_[uniqueId] = nativeWindow;
+        return GSERROR_OK;
+    }
+    return GSERROR_OK;
+}
+
+SurfaceError SurfaceUtils::RemoveNativeWindow(uint64_t uniqueId)
+{
+    std::lock_guard<std::mutex> lockGuard(nativeWindowLock_);
+    nativeWindowCache_.erase(uniqueId);
+    return GSERROR_OK;
+}
+} // namespace OHOS
