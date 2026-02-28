ce6c536920efeec22aa7aa57bab1f76a701d5f18
lvshiqi1 <lvshiqi1@h-partners.com>
2026-02-28 11:33:58 +0800

    修复视频裁剪
    Signed-off-by: lvshiqi1 <lvshiqi1@h-partners.com>
    Change-Id: I8b9986774465a404ea368092eca181feb376f4ab

--- graphic_2d/android/rs_surface_texture_android.cpp
+++ graphic_2d/android/rs_surface_texture_android.cpp
@@ -17,6 +17,8 @@
 
 #include <cstdint>
 #include <securec.h>
+#include <cmath>
+#include <limits>
 
 #include "platform/common/rs_log.h"
 const float TRANSLATE_VALUE = 0.5f;
@@ -66,8 +68,29 @@ void AndroidSurfaceTexture::UpdateTransform()
 
 void AndroidSurfaceTexture::UpdateSurfaceDefaultSize(float width, float height)
 {
-    width_ = width;
-    height_ = height;
+    const int pixelAlignment = 16;
+    int widthInt = static_cast<int>(width + 0.5f);
+    int heightInt = static_cast<int>(height + 0.5f);
+
+    bool widthNeedsAlignment = (widthInt % pixelAlignment != 0);
+    bool heightNeedsAlignment = (heightInt % pixelAlignment != 0);
+    bool incomingHasUnaligned = (widthNeedsAlignment || heightNeedsAlignment);
+
+    int savedWidthInt = static_cast<int>(width_ + 0.5f);
+    int savedHeightInt = static_cast<int>(height_ + 0.5f);
+    bool savedWidthAligned = (savedWidthInt % pixelAlignment == 0);
+    bool savedHeightAligned = (savedHeightInt % pixelAlignment == 0);
+    bool savedIsFullyAligned = (savedWidthAligned && savedHeightAligned);
+
+    bool shouldSave = (width_ == 0 && height_ == 0) ||
+        (incomingHasUnaligned && savedIsFullyAligned);
+
+    if (shouldSave) {
+        width_ = width;
+        height_ = height;
+    } else {
+        return;
+    }
 }
 
 bool AndroidSurfaceTexture::InitializeTextureIfNeeded()
@@ -77,7 +100,6 @@ bool AndroidSurfaceTexture::InitializeTextureIfNeeded()
             return false;
         }
         glGenTextures(1, &textureId_);
-        ROSEN_LOGE("AndroidSurfaceTexture::DrawTextureImage attachCallback textureId %{public}d", textureId_);
         attachCallback_(textureId_, true);
         state_ = AttachmentState::ATTACHED;
     }
@@ -86,12 +108,11 @@ bool AndroidSurfaceTexture::InitializeTextureIfNeeded()
 
 std::shared_ptr<Drawing::Image> AndroidSurfaceTexture::CreateTextureImage(RSPaintFilterCanvas& canvas)
 {
-    ROSEN_LOGE("AndroidSurfaceTexture::textureId_ %{public}d", textureId_);
     auto image = std::make_shared<Drawing::Image>();
     if (image == nullptr) {
-        ROSEN_LOGE("create Drawing image fail");
         return nullptr;
     }
+
     Drawing::TextureInfo textureInfo;
     textureInfo.SetWidth((int)1);
     textureInfo.SetHeight((int)1);
@@ -101,6 +122,7 @@ std::shared_ptr<Drawing::Image> AndroidSurfaceTexture::CreateTextureImage(RSPain
     textureInfo.SetFormat(GL_RGBA8_OES);
     Drawing::BitmapFormat fmt =
         Drawing::BitmapFormat{ Drawing::COLORTYPE_RGBA_8888, Drawing::ALPHATYPE_PREMUL };
+
     bool ret = image->BuildFromTexture(*canvas.GetGPUContext(), textureInfo,
         Drawing::TextureOrigin::TOP_LEFT, fmt, nullptr);
     if (!ret) {
@@ -109,35 +131,72 @@ std::shared_ptr<Drawing::Image> AndroidSurfaceTexture::CreateTextureImage(RSPain
     return image;
 }
 
-void AndroidSurfaceTexture::DrawTextureImage(RSPaintFilterCanvas& canvas, bool freeze, const Drawing::Rect& clipRect)
+bool AndroidSurfaceTexture::ApplyClipForAlignment(RSPaintFilterCanvas& canvas)
 {
-    if (state_ == AttachmentState::DETACHED || attachCallback_ == nullptr || updateCallback_ == nullptr) {
-        return;
+    if (width_ <= 0 || height_ <= 0) {
+        return true;
     }
-    if (!InitializeTextureIfNeeded()) {
-        return;
+    const int pixelAlignment = 16;
+    int checkWidth = static_cast<int>(width_ + 0.5f);
+    int checkHeight = static_cast<int>(height_ + 0.5f);
+    int alignedWidth = ((checkWidth + pixelAlignment - 1) / pixelAlignment) * pixelAlignment;
+    int alignedHeight = ((checkHeight + pixelAlignment - 1) / pixelAlignment) * pixelAlignment;
+    bool widthNeedsPadding = (alignedWidth > checkWidth);
+    bool heightNeedsPadding = (alignedHeight > checkHeight);
+    if (widthNeedsPadding || heightNeedsPadding) {
+        float rightClip = widthNeedsPadding ? static_cast<float>(checkWidth) / alignedWidth : 1.0f;
+        float bottomClip = heightNeedsPadding ? static_cast<float>(checkHeight) / alignedHeight : 1.0f;
+        Drawing::Rect videoRect(0.0f, 0.0f, rightClip, bottomClip);
+        canvas.ClipRect(videoRect, Drawing::ClipOp::INTERSECT, false);
+        return true;
     }
+    return false;
+}
 
-    bool bufferAvailable = bufferAvailable_.load();
-    if (!freeze && bufferAvailable_.load()) {
-        UpdateTransform();
-        bufferAvailable_.store(false);
-    }
-
-    auto image = CreateTextureImage(canvas);
-    if (image == nullptr) {
-        return;
-    }
-
-    canvas.Save();
-    canvas.Translate(clipRect.GetLeft(), clipRect.GetTop());
-    canvas.Scale(clipRect.GetWidth(), clipRect.GetHeight());
+bool AndroidSurfaceTexture::ApplyTransformMatrix(RSPaintFilterCanvas& canvas)
+{
     if (!transform_.IsIdentity()) {
+        Drawing::Matrix::Buffer matrixBuffer;
+        transform_.GetAll(matrixBuffer);
         Drawing::Matrix transformAroundCenter(transform_);
         transformAroundCenter.PreTranslate(-TRANSLATE_VALUE, -TRANSLATE_VALUE);
         transformAroundCenter.PostScale(1, -1);
         transformAroundCenter.PostTranslate(TRANSLATE_VALUE, TRANSLATE_VALUE);
         canvas.ConcatMatrix(transformAroundCenter);
+        return true;
+    }
+    return false;
+}
+
+void AndroidSurfaceTexture::DrawTextureImage(RSPaintFilterCanvas& canvas, bool freeze, const Drawing::Rect& clipRect)
+{
+    if (state_ == AttachmentState::DETACHED || attachCallback_ == nullptr || updateCallback_ == nullptr) {
+        return;
+    }
+    auto x = clipRect.GetLeft();
+    auto y = clipRect.GetTop();
+    auto width = clipRect.GetWidth();
+    auto height = clipRect.GetHeight();
+    if (!InitializeTextureIfNeeded()) {
+        return;
+    }
+    bool bufferAvailable = bufferAvailable_.load();
+    if (!freeze && bufferAvailable_.load()) {
+        UpdateTransform();
+        bufferAvailable_.store(false);
+    }
+    auto image = CreateTextureImage(canvas);
+    if (image == nullptr) {
+        return;
+    }
+    canvas.Save();
+    canvas.Translate(x, y);
+    canvas.Scale(width, height);
+    if (!ApplyClipForAlignment(canvas)) {
+        return;
+    }
+    if (!ApplyTransformMatrix(canvas)) {
+        return;
     }
     canvas.DrawImage(*image, 0, 0, Drawing::SamplingOptions());
     canvas.Restore();
