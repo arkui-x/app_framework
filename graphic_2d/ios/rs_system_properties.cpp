3f1207c8e50ef4195e66c443fe18256698242aa0
txzhang <zhangtianxiang4@huawei.com>
2026-04-16 14:03:22 +0800

    arkui-x update master OH to weekly_20260413 and arkui-x fix build bug
    Signed-off-by: txzhang <zhangtianxiang4@huawei.com>
    Change-Id: I52944392801b35240602f30927bd5953f73fa748

--- graphic_2d/ios/rs_system_properties.cpp
+++ graphic_2d/ios/rs_system_properties.cpp
@@ -506,5 +506,15 @@ bool RSSystemProperties::GetTransactionDataTraceEnabled()
 {
     return false;
 }
+
+bool RSSystemProperties::GetUnmarshalParallelEnabled()
+{
+    return false;
+}
+
+uint32_t RSSystemProperties::GetUnmarshalParallelMinDataSize()
+{
+    return 0;
+}
 } // namespace Rosen
 } // namespace OHOS
